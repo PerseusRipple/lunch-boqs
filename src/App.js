2ee15445408f8888d83386b431406aa92f8cdc8b
PerseusRipple <perseus.ripple@gmail.com>
2019-03-08 15:45:24 -0500

    added basic nav main and footer html and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+
+import axios from 'axios'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <section className='topnav'>
+          {/* <a class='active' href='#home'>
+            Home
+          </a> */}
+          <section className='login-container'>
+            <button type='submit'>Login</button>
+            <button type='submit'>Sign Up</button>
+            {/* <form action='/action_page.php'>
+              <input type='text' placeholder='Username' name='username' />
+              <input type='text' placeholder='Password' name='psw' /> 
+            </form> */}
+          </section>
+        </section>
+
+        <section className='main-container'>
+          <h1 className='company-box'>LunchBoqs</h1>
+        </section>
+        <section className='bottom-container' />
+        <footer className='footer'>Made with 💝 from SDG</footer>
+      </>
+    )
   }
 }
 
