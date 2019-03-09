6ab5aac605dcd7ec398c2e39c4ecbe2977682e5e
PerseusRipple <perseus.ripple@gmail.com>
2019-03-09 16:42:05 -0500

    logo and images show

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import axios from 'axios'
 
 class App extends Component {
@@ -10,6 +9,12 @@ class App extends Component {
           {/* <a class='active' href='#home'>
             Home
           </a> */}
+          <img
+            src='./images/logo_transparent.png'
+            // alt='company logo'
+            width='130'
+            height='90'
+          />
           <section className='login-container'>
             <button type='submit'>Login</button>
             <button type='submit'>Sign Up</button>
@@ -20,11 +25,31 @@ class App extends Component {
           </section>
         </section>
 
-        <section className='main-container'>
-          <h1 className='company-box'>LunchBoqs</h1>
+        <section className='content'>
+          <section className='flex-container'>
+            <section className='company-box'>
+              <section className='image-content'>
+                <a href='#'>
+                  {/* <img
+                scr='./images/logo.png'
+                alt='LOGO'
+                width='560'
+                height='560'
+              /> */}
+                </a>
+              </section>
+              <h2>Keep Midday Simple.</h2>
+            </section>
+          </section>
+          <section className='bottom-container' />
         </section>
-        <section className='bottom-container' />
-        <footer className='footer'>Made with 💝 from SDG 2019</footer>
+        <footer className='footer'>
+          Made with{' '}
+          <span role='img' alt='heart emoji'>
+            💝
+          </span>{' '}
+          from SDG 2019
+        </footer>
       </>
     )
   }
